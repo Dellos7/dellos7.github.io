dab7bf4c1b8052a188ef3861d9aeded249f09c2b
David López Castellote <david.lopez.castellote@gmail.com>
2020-05-27 18:36:25 +0200

    fix dlc-css-terminal

--- src/components/dlc-terminal/dlc-terminal.tsx
+++ src/components/dlc-terminal/dlc-terminal.tsx
@@ -3,26 +3,25 @@ import { Component, h } from '@stencil/core';
 
 @Component({
     tag: 'dlc-terminal',
-    styleUrl: 'dlc-terminal.scss',
-    shadow: true
+    styleUrl: 'dlc-terminal.scss'
 })
 export class DlcTerminal {
 
     render() {
         return (
             <div class="wrapper">
-            <dlc-cssterminal shell="~ david$">
-                <dlc-cssterminal-block command="pwd">
-                    /the/internet/dlopezcastellote.dev
-    </dlc-cssterminal-block>
-                <dlc-cssterminal-block command="echo $USER" delay={4}>
-                    David López Castellote
-    </dlc-cssterminal-block>
-                <dlc-cssterminal-block command="whoami" delay={15}>
-                    Programador y proyecto de futuro profesor 💻 👨‍🏫
-    </dlc-cssterminal-block> 
-                <dlc-cssterminal-block shell="~ david$" command="&nbsp;" delay={26}></dlc-cssterminal-block>
-            </dlc-cssterminal>
+                <dlc-cssterminal shell="~ david$">
+                    <dlc-cssterminal-block command="pwd">
+                        /the/internet/dlopezcastellote.dev
+                    </dlc-cssterminal-block>
+                    <dlc-cssterminal-block command="echo $USER" delay={4}>
+                        David López Castellote
+                    </dlc-cssterminal-block>
+                    <dlc-cssterminal-block command="whoami" delay={15}>
+                        Programador y proyecto de futuro profesor 💻 👨‍🏫
+                    </dlc-cssterminal-block> 
+                    <dlc-cssterminal-block shell="~ david$" command="&nbsp;" delay={26}></dlc-cssterminal-block>
+                </dlc-cssterminal>
             </div>
         );
     }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { Components } from './components';
 import '@stencil/router';
 import 'ionicons';
-import 'dlc-css-terminal';
+ import 'dlc-css-terminal';
