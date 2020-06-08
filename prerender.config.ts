828db9fa865e157c7c398dc65d80a42d10a2005f
David López Castellote <david.lopez.castellote@gmail.com>
2020-06-08 11:16:09 +0200

    comentario prerender config urls

--- prerender.config.ts
+++ prerender.config.ts
@@ -1,4 +1,5 @@
 import { PrerenderConfig } from '@stencil/core';
 export const config: PrerenderConfig = {
+    // Listar las urls que no se enlazan directamente en la web pero que queremos que se haga prerender
     entryUrls: [ '/', '/contact-form-error', '/contact-form-submited' ]
 };
