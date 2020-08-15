727416846606a6064d593cb6596f2b1e320d23e5
David López Castellote <david.lopez.castellote@gmail.com>
2020-08-15 13:31:43 +0200

    script para pasar un post de dev a prod y anyadidos los 2 primeros articulos

---
+++ scripts/fromDevToProd.js
@@ -0,0 +1,73 @@
+const fs = require('fs-extra');
+const Remarkable = require('remarkable');
+const md = new Remarkable();
+md.set({
+    html: true
+});
+
+const args = process.argv.slice(2);
+const devConfigJsonFile = 'blog.config.dev.json';
+const prodConfigJsonFile = 'blog.config.json';
+const [mdFile] = args;
+if( mdFile ){
+    readConfFiles()
+    .then( async( [devConfigJsonFileContent, prodConfigJsonFileContent] ) => {
+        let fileContent = fs.readFileSync(`${mdFile}`, 'utf8');
+        let contentHtml = md.render(fileContent);
+        // Get the image names of the post
+        const imagesNames = getImagesNames(contentHtml);
+        const imagesNamesDev = imagesNames.map( (name) => `${devConfigJsonFileContent.images_src_dir}/${name}` );
+        const imagesNamesProd = imagesNames.map( (name) => `${prodConfigJsonFileContent.images_src_dir}/${name}` );
+        // Copy all the files of the post from the dev dir to the prod dir
+        for( let i = 0; i < imagesNames.length; i++ ){
+            fs.copyFileSync( imagesNamesDev[i], imagesNamesProd[i] );
+        }
+        console.log(`-> ${imagesNames.length} copied from ${devConfigJsonFileContent.images_src_dir} to ${prodConfigJsonFileContent.images_src_dir}`);
+        const mdFileName = mdFile.substring( mdFile.lastIndexOf('/') + 1 );
+        // Copy the markdown post file
+        fs.copyFileSync( mdFile, `${prodConfigJsonFileContent.blog_src_dir}/${mdFileName}` );
+        console.log(`-> Blog file copied: ${mdFile} to ${prodConfigJsonFileContent.blog_src_dir}/${mdFileName}`);
+    });
+
+} else{
+    console.error( 'No blog development markdown file provided.' );
+}
+
+
+function readConfFiles() {
+    const devConfigFileProm = new Promise( (resolve, reject) => {
+        fs.readFile(devConfigJsonFile, 'utf-8', (err, data) => {
+            if( err ) {
+                const error = `Something went wrong when reading the ${devConfigJsonFile} config file: ${err}`;
+                reject(error);
+            }
+            else {
+                resolve(JSON.parse(data));
+            }
+        });
+    });
+    const prodConfigFileProm = new Promise( (resolve, reject) => {
+        fs.readFile(prodConfigJsonFile, 'utf-8', (err, data) => {
+            if( err ) {
+                const error = `Something went wrong when reading the ${prodConfigJsonFile} config file: ${err}`;
+                reject(error);
+            }
+            else {
+                resolve(JSON.parse(data));
+            }
+        });
+    });
+    return Promise.all( [devConfigFileProm, prodConfigFileProm] );
+}
+
+function getImagesNames( contentHtml ){
+    const getImageTagAndSrcRegexp = new RegExp(/<img(.|\s)*?src="(.|\s)*?"/, 'ig');
+    const imagesArr = contentHtml.match( getImageTagAndSrcRegexp );
+    let imageNamesArr = [];
+    for( const img of imagesArr ){
+        const imagePath = img.substring( img.indexOf(`src="`) + 5, img.length-1 );
+        const imageName = imagePath.substring( imagePath.lastIndexOf('/') + 1 );
+        imageNamesArr.push(imageName);
+    }
+    return imageNamesArr;
+}
